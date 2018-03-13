50636e1df8d2a4762106bf315c71b129153bb882
Ben Pearson <ben@rdydev.com>
2018-03-13 20:23:39 +0000

    refactor the flickr presenter to remove some of the duplicate code

--- app/src/main/java/com/rdydev/flickr/gallery/FlickrPresenter.kt
+++ app/src/main/java/com/rdydev/flickr/gallery/FlickrPresenter.kt
@@ -2,12 +2,14 @@ package com.rdydev.flickr.gallery
 
 import com.rdydev.flickr.gallery.data.DataModule
 import com.rdydev.flickr.gallery.data.FlickrApi
+import com.rdydev.flickr.gallery.data.model.FlickrItem
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 internal class FlickrPresenter {
 
-    private lateinit var view: FlickrView
+    private var view: FlickrView
     private val flickrApi: FlickrApi
 
     constructor(view: FlickrView) : this(view, DataModule.provideFlickrApi())
@@ -20,8 +22,26 @@ internal class FlickrPresenter {
     fun fetchData() {
         view.showLoading()
 
-        flickrApi.getFeed()
-                .subscribeOn(Schedulers.io())
+        watch(flickrApi.getFeed())
+    }
+
+    fun searchTags(tags: String?) {
+        if (tags.isNullOrEmpty()) {
+            fetchData()
+            return
+        }
+
+        tags?.let {
+            if ("[A-Za-z,]+".toRegex().matches(tags)) {
+                watch(flickrApi.search(tags))
+            } else {
+                view.onError("Only A-Z a-z and , are accepted as search input")
+            }
+        }
+    }
+
+    private fun watch(source: Single<List<FlickrItem>>) {
+        source.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         { data ->
@@ -33,33 +53,6 @@ internal class FlickrPresenter {
                             view.onError(error.localizedMessage)
                         }
                 )
-    }
-
-    fun searchTags(tags: String?) {
-        if (tags.isNullOrEmpty()) {
-            fetchData()
-            return
-        }
-
-        tags?.let {
-            if ("[A-Za-z,]+".toRegex().matches(tags)) {
-                flickrApi.search(tags)
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(
-                                { data ->
-                                    view.hideLoading()
-                                    view.onData(data)
-                                },
-                                { error ->
-                                    view.hideLoading()
-                                    view.onError(error.localizedMessage)
-                                }
-                        )
-            } else {
-                view.onError("Only A-Z a-z and , are accepted as search input")
-            }
-        }
     }
 
 }
